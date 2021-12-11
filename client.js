1be053da98f3e0fb93e555dd88cec094c3daf9f4
Leah Grim <grimchelate@gmail.com>
2021-12-11 17:00:02 -0600

    added a global varible EmployeeObject to store the employee info inputs

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
 $(document).ready(readyNow);
- 
+ //global list of employee & info
+let employeeRegistrar = [];
+
 function readyNow(){
     console.log('Thanks for working with our Hospital, Here is your salary');
    
@@ -12,16 +14,16 @@ function readyNow(){
 function onAddEmployee(event) {
     // prevent form from page reloading 
   event.preventDefault();
-    console.log('employee added 💰');
+    
+  console.log('employee added 💰');
 
-   //grab data from DOM 
-   //from form inputs
-let firstName = $('#firstNameInput').val();
-let lastName = $('#lastNameInput').val();
-let idNumber= Number($('#iDNumberInput').val());
-let title = $('#titleInput').val();
-let annualSalary = Number($('#AnnualSalaryInput').val());
-
+   //grab data from DOM in form inputs to be used as objects of employee attributes
+    let firstName = $('#firstNameInput').val();
+    let lastName = $('#lastNameInput').val();
+    let idNumber= Number($('#iDNumberInput').val());
+    let title = $('#titleInput').val();
+    let annualSalary = Number($('#AnnualSalaryInput').val());
+   //box up each employee info into an object array
     let employeeObject = {
         firstName: firstName, 
         lastName: lastName,
@@ -30,10 +32,14 @@ let annualSalary = Number($('#AnnualSalaryInput').val());
         annualSalary: annualSalary 
     };
     console.log('New Employee Calculation:', employeeObject)
+   // clear the inputs once submit is pushed
+    firstName = $('#firstNameInput').val('');
+    lastName = $('#lastNameInput').val('');
+    idNumber= $('#iDNumberInput').val('');
+    title = $('#titleInput').val('');
+    annualSalary = $('#AnnualSalaryInput').val('');
 
-firstName = $('#firstNameInput').val('');
-lastName = $('#lastNameInput').val('');
-idNumber= $('#iDNumberInput').val('');
-title = $('#titleInput').val('');
-annualSalary = $('#AnnualSalaryInput').val('');
+  //push the new employee info added to the global array
+    employeeRegistrar.push(employeeObject);
 }
+
