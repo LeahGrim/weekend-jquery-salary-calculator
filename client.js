4c7881f4fecb79766f31e40f00078e07bfef4af1
Leah Grim <grimchelate@gmail.com>
2021-12-11 16:51:02 -0600

    values now able to be added via submit button to create object in console

--- client.js
+++ client.js
@@ -1,5 +1,39 @@
-$(document).ready(readyNow());
+$(document).ready(readyNow);
  
 function readyNow(){
-    console.log('JQ');
+    console.log('Thanks for working with our Hospital, Here is your salary');
+   
+ $('#employeeForm').on('submit', onAddEmployee);
+
+
+}
+
+
+function onAddEmployee(event) {
+    // prevent form from page reloading 
+  event.preventDefault();
+    console.log('employee added 💰');
+
+   //grab data from DOM 
+   //from form inputs
+let firstName = $('#firstNameInput').val();
+let lastName = $('#lastNameInput').val();
+let idNumber= Number($('#iDNumberInput').val());
+let title = $('#titleInput').val();
+let annualSalary = Number($('#AnnualSalaryInput').val());
+
+    let employeeObject = {
+        firstName: firstName, 
+        lastName: lastName,
+        idNumber: idNumber,
+        title: title, 
+        annualSalary: annualSalary 
+    };
+    console.log('New Employee Calculation:', employeeObject)
+
+firstName = $('#firstNameInput').val('');
+lastName = $('#lastNameInput').val('');
+idNumber= $('#iDNumberInput').val('');
+title = $('#titleInput').val('');
+annualSalary = $('#AnnualSalaryInput').val('');
 }
